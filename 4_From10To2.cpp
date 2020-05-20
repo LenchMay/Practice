c469a44e37ecf7533a9f49089586f2f5ba1444fd
LenchMay <56229588+LenchMay@users.noreply.github.com>
2020-05-20 16:57:51 +0300

    Add files via upload

---
+++ 4_From10To2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int num = 0, result = 0, discharge = 1;
+	cout << "Transfer from the 10th number system to the 2nd system of value. Enter an integer:" << endl;
+	cin >> num;
+	do {
+		result += (num % 2) * discharge;
+		num /= 2;
+		discharge *= 10;
+	} while (num > 0);
+	cout << result << endl;
+}
