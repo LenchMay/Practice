8efd4e8d8a40ea07d2be5236cbf2f4e1a0edda8d
LenchMay <56229588+LenchMay@users.noreply.github.com>
2020-05-20 21:52:51 +0300

    Add files via upload

---
+++ 7_Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1 = 1;
+	int num2 = 2;
+	int sum = 2;
+	do {
+		num1 += num2;
+		num2 += num1;
+		if (num1 % 2 == 0)
+			sum += num1;
+		if (num2 % 2 == 0)
+			sum += num2;
+		//cout << num1 << " " << num2 << " " << sum << endl;
+	} while (num2 < 4000000);
+	std::cout << sum << endl;
+}
