2212dee06493b588774e9fce2302165712bebb31
LenchMay <56229588+LenchMay@users.noreply.github.com>
2020-05-20 16:55:41 +0300

    Add files via upload

---
+++ 3_GreatestCommonFactor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int a, b;
+	cout << "Finding the greatest common factor. Enter two integers separated by spaces:" << endl;
+	cin >> a >> b;
+	do {
+		if (abs(a) > abs(b))
+			a %= b;
+		else
+			b %= a;
+	} while (a != 0 && b != 0);
+	cout << a + b << endl;
+}
