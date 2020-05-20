f1ea6d41afd486440916ec68e1f8a4abfc962660
LenchMay <56229588+LenchMay@users.noreply.github.com>
2020-05-20 17:31:53 +0300

    Add files via upload

---
+++ 6_IsPalindrom.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool IsPalindrom(string link) {
+	string t;
+	for (int i = link.size() - 1; i >= 0; --i)
+		if (link[i] != ' ')
+			t += link[i];
+	if (t == link)
+		return true;
+	else
+		return false;
+}
+
+void main() {
+	string s;
+	cout << "Check for a palindrome. Enter the string:" << endl;
+	cin >> s;
+	cout << IsPalindrom(s) << endl;
+}
